7c7d9a4d044ee25bb1c8d108841796d66f291a68
computerbox <computerbox124@gmail.com>
2021-06-08 23:20:25 +0400

    Added simple main-menu

--- main.c
+++ main.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+//#include <all-structs.h>
+//#include <search-and-filter.h>
+//#include <delete.h>
+//#include <sort.h>
 
+struct menu
+{
+  char *name;
+}main_menu[20];
+
+
+int main(){
+main_menu[0].name="Exit";
+int size=1;
+while(1)
+{
+  printf("Welcome to the student management system of ADA university !\nPlease , choose one of the options by typing the number.\n");
+  for(int i=0;i<size;i++)
+  {
+    printf("%d) %s\n",i+1,main_menu[i].name);
+  }
+  int x;
+  scanf("%d",&x);
+}
+
+
+
+return 0;
+}
