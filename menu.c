77fe7cbb273721b685fd7702a1d057428042745f
computerbox <computerbox124@gmail.com>
2021-06-26 13:27:09 +0400

    * Updated menu * Updated database (fixed)

--- menu.c
+++ menu.c
@@ -6,19 +6,22 @@ void main_menu()
 {
  int size=1;
  while(1) {
-  printf("============Welcome to the student management system of ADA university!==========\n==============Please, choose one of the options by typing the number.==========\n");
+  printf("============Welcome to the student management system of ADA university!==========\n");
   puts("");
-  printf(">>  To list all students--> 1\n");
-  printf(">>  To add student info--> 2\n");
-  printf(">>  To delete student info--> 3\n");
-  printf(">>  To create a new item--> 4\n");
-  printf(">>  To edit student info-> 5\n");
-  printf(">>  To exit the process--> 0\n");
+  printf(">>  To list all students please type 1\n");
+  printf(">>  To add student information please type 2\n");
+  printf(">>  To edit student information type 3\n");
+  printf(">>  To exit from the program please type 4\n");
   int x;
   scanf("%d",&x);
+  switch(x){
+    case 1:
 
-  if (x==0) return ;
-  if (x==1) {
+    case 2:
+
+    case 3:
+
+    case 4:
 
   }
  }
