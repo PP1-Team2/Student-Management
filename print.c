b1629ed54a1eb6e132fdb2e91f0140b7d0186913
computerbox <computerbox124@gmail.com>
2021-06-26 12:49:21 +0400

    Updated print and database

--- print.c
+++ print.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
- int main(){
+
+void print_all(){
 
     FILE *file;
     char filename[100000], database;
 
-    file=fopen("input.txt", "r");
+    file=fopen("students.dat", "r");
     if (file==NULL){
         printf("Not Found");
         exit(0);
@@ -17,5 +18,4 @@
         database=fgetc(file);
 }
     fclose(file);
-    return 0;
 }
