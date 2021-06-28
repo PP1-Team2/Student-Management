2884a6985b3eb5876c333cafa9b58fe53bdcb9d2
computerbox <computerbox124@gmail.com>
2021-06-28 23:51:42 +0400

    * Updated

--- search-and-filter.c
+++ search-and-filter.c
@@ -47,8 +47,8 @@ int cmp(Student *a,Student *b)
   if(a->number_of_subjects!=0)
   {
     if(a->number_of_subjects!=b->number_of_subjects)return 0;
-    sort_subjects_by_string(a->subjects,a->number_of_subjects);
-    sort_subjects_by_string(b->subjects,a->number_of_subjects);
+    //sort_subjects_by_string(a->subjects,a->number_of_subjects);
+    //sort_subjects_by_string(b->subjects,b->number_of_subjects);
     for(int i=0;i<(a->number_of_subjects);i++)
     {
      if(checksubject(&a->subjects[i],&b->subjects[i])==0)return 0;
